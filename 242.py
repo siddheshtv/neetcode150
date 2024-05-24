5f72d625202e02810bb04a456fcc60e01d64e670
siddheshtv <siddheshkulthe43@gmail.com>
2024-05-24 23:03:09 +0530

    242

---
+++ 242.py
@@ -0,0 +1,8 @@
+# Is Anagram
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ss = list(s)
+        tt = list(t)
+        ss.sort()
+        tt.sort()
+        return ss == tt
